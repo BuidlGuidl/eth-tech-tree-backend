050f7325d4a365c1ded6341404f81f7f551de4f0
escottalexander@gmail.com <escottalexander@gmail.com>
2024-08-08 07:42:54 -0400

    fix challenge slug middleware

--- packages/server/utils/middleware.ts
+++ packages/server/utils/middleware.ts
@@ -1,12 +1,22 @@
 import { param, validationResult } from "express-validator";
 import { Request, Response, NextFunction } from "express";
 import { ALLOWED_NETWORKS } from ".";
+import { fetchChallengeNames } from "../services/challenge";
+
+const isChallengeSlug = async (challengeSlug: string) => {
+  const challengeNames = await fetchChallengeNames();
+  return challengeNames.includes(challengeSlug);
+};
 
 /**
  * Validations for the params of the challenge submission route
  */
-export const validateSubmission = [
-  param("challengeId").isInt().withMessage("Challenge ID must be an integer"),
+export const validateChallengeSubmission = [
+  param("challengeSlug").custom(async slug => {
+    if (!await isChallengeSlug(slug)) {
+      throw new Error(`Challenge "${slug}" not found.`);
+    }
+  }),
   param("network").isIn(ALLOWED_NETWORKS).withMessage("Invalid network name"),
   param("address").isEthereumAddress().withMessage("Invalid Ethereum address"),
   (req: Request, res: Response, next: NextFunction) => {
@@ -17,3 +27,14 @@ export const validateSubmission = [
     next();
   },
 ];
+
+export const validateAddress = [
+  param("address").isEthereumAddress().withMessage("Invalid Ethereum address"),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+]
